2d5a3b475d3363b957dbe1252823c6d1312a8924
lwrb172 <lwrbielak@gmail.com>
2024-05-25 00:15:29 +0200

    outline boundary

--- snake.c
+++ snake.c
@@ -3,7 +3,24 @@
 
 #include "conio.h"
 
+int height = 30;
+int width = 30, gameover, score;
+
+void draw() {
+	for (int i = 0; i < height; ++i) {
+		for (int j = 0; j < width; ++j) {
+			if (i == 0 || i == width - 1 || j == 0 || j == height - 1) {
+				printf("#");
+			} else {
+				printf(" ");
+			} 
+		}
+		printf("\n");
+	}
+}
+
 int main() {
+	draw();
 
 	
 	return 0;
